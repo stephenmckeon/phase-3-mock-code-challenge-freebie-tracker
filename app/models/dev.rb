ca2b8f661fb242d7d10622c8c5c9f004196c97b7
Stephen McKeon <stephen.mckeon@powerhrg.com>
2024-07-23 10:06:04 -0400

    Allow dev to give away freebie

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -5,4 +5,10 @@ class Dev < ActiveRecord::Base
   def received_one?(item_name)
     freebies.where(item_name: item_name).present?
   end
+
+  def give_away(dev, freebie)
+    return if freebie.dev != self
+
+    freebie.update(dev: dev)
+  end
 end
