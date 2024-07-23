1094da0ecedd27df2c788b238da7d3e3dd950255
Stephen McKeon <stephen.mckeon@powerhrg.com>
2024-07-23 09:50:55 -0400

    Check if dev received an item

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,4 +1,8 @@
 class Dev < ActiveRecord::Base
   has_many :freebies
   has_many :companies, through: :freebies
+
+  def received_one?(item_name)
+    freebies.where(item_name: item_name).present?
+  end
 end
