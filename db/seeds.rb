0041489e56c6281de9db0ac36518d78ac7e6acbb
Stephen McKeon <stephen.mckeon@powerhrg.com>
2024-07-23 09:09:28 -0400

    Add a freebie to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,22 @@
+Company.destroy_all
+Dev.destroy_all
+Freebie.destroy_all
+
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
+company = Company.create(name: "Google", founding_year: 1998)
 Company.create(name: "Facebook", founding_year: 2004)
 Company.create(name: "Dunder Mifflin", founding_year: 2002)
 Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
+dev = Dev.create(name: "Rick")
 Dev.create(name: "Morty")
 Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
-# ***************************************************************
-# * TODO: create freebies! Remember, a freebie belongs to a dev *
-# * and a freebie belongs to a company.                         *
-# ***************************************************************
-# Create freebies Here
+Freebie.create(company: company, dev: dev,
+               item_name: "Name", value: 10)
 
 puts "Seeding done!"
