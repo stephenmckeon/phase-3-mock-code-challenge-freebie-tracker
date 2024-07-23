da7b9994d8ec3010cc43167d29e6d30953ac3de4
Stephen McKeon <stephen.mckeon@powerhrg.com>
2024-07-23 09:35:32 -0400

    Give away freebies

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,10 @@
 class Company < ActiveRecord::Base
   has_many :freebies
   has_many :devs, through: :freebies
+
+  def give_freebie(dev, item_name, value)
+    # Freebie.create(company: self, dev: dev, item_name: item_name, value: value)
+    # same as...
+    freebies.create(dev: dev, item_name: item_name, value: value)
+  end
 end
